336d6bedfc0ce277882584abe9b7ee5fa66a5954
Deniz Ata EŞ <85442526+denizataes@users.noreply.github.com>
2022-12-30 00:50:30 +0300

    Initial Commit

---
+++ CustomTableViewSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomTableViewSwift
+//
+//  Created by Deniz Ata Eş on 30.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
